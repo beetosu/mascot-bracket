76f5bbfdb1adf826289baff51fdef75c50fda7f5
beetosu <cpwalsh@buffalo.edu>
2023-11-25 14:01:02 -0500

    Add comments

--- app/common/tournament-round-enum.tsx
+++ app/common/tournament-round-enum.tsx
@@ -8,6 +8,11 @@ enum TournamentRound {
 	Winner = 126
 }
 
+/**
+ * Maps a round enum with the enum representing the next round.
+ * @param currentRound the round we want to find the next round for.
+ * @returns the round that is the next round, relative to the currentRound.
+ */
 export function getNextRound(currentRound: TournamentRound): TournamentRound | null {
   switch (currentRound) {
     case TournamentRound.First:
@@ -29,6 +34,11 @@ export function getNextRound(currentRound: TournamentRound): TournamentRound | n
   }
 }
 
+/**
+ * Map the round enum to a xStep value.
+ * @param currentRound The round we want to find the xStep for.
+ * @returns The xStep associated with the round.
+ */
 export function getXStep(currentRound: TournamentRound): number {
   switch (currentRound) {
     case TournamentRound.First:
@@ -50,6 +60,11 @@ export function getXStep(currentRound: TournamentRound): number {
   }
 }
 
+/**
+ * Map the round enum to a yStart value.
+ * @param currentRound The round we want to find the yStart for.
+ * @returns The yStart associated with the round.
+ */
 export function getYStart(currentRound: TournamentRound): number {
   switch (currentRound) {
     case TournamentRound.First:
@@ -71,6 +86,11 @@ export function getYStart(currentRound: TournamentRound): number {
   }
 }
 
+/**
+ * Map the round enum to a yGap value.
+ * @param currentRound The round we want to find the yGap for.
+ * @returns The yGap associated with the round.
+ */
 export function getYGap(currentRound: TournamentRound): number {
   switch (currentRound) {
     case TournamentRound.First:
@@ -92,6 +112,11 @@ export function getYGap(currentRound: TournamentRound): number {
   }
 }
 
+/**
+ * Map the round enum to a yStep value.
+ * @param currentRound The round we want to find the yStep for.
+ * @returns The yStep associated with the round.
+ */
 export function getYStep(currentRound: TournamentRound): number {
   switch (currentRound) {
     case TournamentRound.First:
--- app/components/states/win-state/win-state.tsx
+++ app/components/states/win-state/win-state.tsx
@@ -11,15 +11,11 @@ export type CollegeCoordinates = {
   y: number
 }
 
-function isRight(idx: number, currentRound: TournamentRound): boolean {
-  const nextRound = getNextRound(currentRound);
-
-  // Winner should be centered.
-  if (!nextRound) return false;
-
-  return idx >= nextRound - ((nextRound - currentRound) / 2);
-}
-
+/**
+ * Build a list of text objects to display on the bracket canvas.
+ * @param matchHistory A list of college names based on their placements in the tournament
+ * @returns A list of objects holding the college name, and their coordinates.
+ */
 function generateCoordinates(matchHistory: string[]): CollegeCoordinates[] {
   const collegeCoords: CollegeCoordinates[] = [];
 
@@ -33,12 +29,19 @@ function generateCoordinates(matchHistory: string[]): CollegeCoordinates[] {
   return collegeCoords;
 }
 
+/**
+ * Determine the x value of a college on the bracket.
+ * @param idx the index of the college in the match history.
+ * @returns the x coordinate associated with the college.
+ */
 function determineX(idx: number): number {
   const currentRound = determineRound(idx);
   const canvasWidth = 1000;
 
+  // x here is either xStep, or the width of the bracket
+  // minus xStep, based on whether the college is on the left
+  // or right hand side.
   let x = getXStep(currentRound);
-
   if (isRight(idx, currentRound)) {
     x = canvasWidth - x;
   }
@@ -46,6 +49,11 @@ function determineX(idx: number): number {
   return x;
 }
 
+/**
+ * Determine the y value of a college on the bracket.
+ * @param idx the index of the college in the match history.
+ * @returns the y coordinate associated with the college.
+ */
 function determineY(idx: number): number {
   const currentRound = determineRound(idx);
   const nextRound = getNextRound(currentRound);
@@ -54,9 +62,17 @@ function determineY(idx: number): number {
 
   const midpoint = (nextRound - currentRound) / 2;
   const quarterpoint = midpoint / 2;
+  // We want to wrap around at the midpoint, since past that the college
+  // will return to to the top, but on the other side of the bracket.
   const relativePosition = (idx - currentRound) % midpoint;
   
-  let y = getYStart(currentRound) + (relativePosition * getYStep(currentRound));
+  // The formula here is:
+  // y = start + step (+ gap if any)
+  const yStep = relativePosition * getYStep(currentRound);
+  let y = getYStart(currentRound) + yStep;
+  // If the college is in the bottom half of the bracket (which would be
+  // the 2nd or 4th quartile of the round), we should account for the gap
+  // between the matches.
   if (relativePosition !== relativePosition % quarterpoint) {
     y += getYGap(currentRound)
   }
@@ -64,10 +80,31 @@ function determineY(idx: number): number {
   return y;
 }
 
+/**
+ * Determine which round a college is currently in.
+ * @param idx The index of the college in the match history.
+ * @returns The round the college is in.
+ */
 function determineRound(idx: number): TournamentRound {
   const enumValues = Object.values(TournamentRound).filter(t => typeof t === 'number') as TournamentRound[];
   
+  // Find the round with the smallest value that the college has past the bounds of.
   return enumValues.findLast(e => e <= idx) ?? TournamentRound.First;
+}
+
+/**
+ * Determine whether the college should be on the left or right hand side of the bracket.
+ * @param idx The index of the college in the match history.
+ * @param currentRound The round the college is currently in.
+ * @returns A boolean representing if the college should be on the right of the bracket.
+ */
+function isRight(idx: number, currentRound: TournamentRound): boolean {
+  const nextRound = getNextRound(currentRound);
+
+  // Winner should be centered.
+  if (!nextRound) return false;
+
+  return idx >= nextRound - ((nextRound - currentRound) / 2);
 }
 
 export default function WinState({ matchHistory }: WinStateProps ) {
