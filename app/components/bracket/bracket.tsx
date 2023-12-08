1b6db9eb26bb1fe6cd7d17f76ca91a62a022541f
beetosu <cpwalsh@buffalo.edu>
2023-12-08 18:31:39 -0500

    #11 show image instead of canvas

--- app/components/bracket/bracket.tsx
+++ app/components/bracket/bracket.tsx
@@ -3,32 +3,40 @@ import styles from './bracket.module.css'
 import { CollegeCoordinates } from '../states/win-state/win-state';
 
 export default function Bracket({collegeCoordinates}: { collegeCoordinates: CollegeCoordinates[] }) {
-	const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	const imageRef = useRef<HTMLImageElement | null>(null);
 
 	useEffect(() => {
-    const canvas = canvasRef.current;
-		if (canvas === null) return;
+		// Create a canvas which we can draw the bracket onto.
+		const canvas = document.createElement('canvas');
 
-    const ctx = canvas.getContext('2d');
+		const ctx = canvas.getContext('2d');
 		if (ctx === null) return;
 
+		canvas.width = 1918;
+		canvas.height = 1338;
 		ctx.font = "22px Arial";
 
-		// set bracket as background image
+		// Set bracket as background image
 		const base = new Image();
 		base.onload = () => {
 			ctx.drawImage(base, 0, 0);
 
-			// draw the college names on top of the background image
+			// Draw the college names on top of the background image
 			for (const c of collegeCoordinates) {
 				c.isRight ? ctx.textAlign = 'right' : ctx.textAlign = 'left';
 				ctx.fillText(c.college, c.x, c.y);
 			}
+
+			// Once the canvas is fully generated, put the content into the image element.
+			const image = imageRef.current;
+			if (image === null) return;
+
+			image.setAttribute('src', canvas.toDataURL('image/png'));
 		}
-		base.src = 'images/bracket.png';		
+		base.src = 'images/bracket.png';
 	});
 
   return (
-    <canvas className={styles.bracket} ref={canvasRef} width={1918} height={1338} />
+	<img alt='A college basketball bracket, with winners predicted by user using marchmascots.com.' className={styles.bracket} ref={imageRef} />
   )
 }
